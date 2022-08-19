9859c6020174b8ec29d76b77eaf23c47a337785b
HekmatAliyaar <101539969+HekmatAliyaar@users.noreply.github.com>
2022-08-20 00:30:48 +0430

    my portifolio is completed

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+// typing animation
+
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Graphic Designer", "Softeware Engineer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true,
+
+});
